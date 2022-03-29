84278b1130ea65422ece6949fcd2071ad89ea155
Paralikar Shravan (MS/ESD-NR6-XC) <Shravan.Paralikar2@in.bosch.com>
2022-03-29 18:52:09 +0530

    Add test

--- AnalogToDigitalConversion.h
+++ AnalogToDigitalConversion.h
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cmath.h>
+#include <cmath>
 
 int GetAnalogToDigitalConversion(int ADC_RESOLUTION);
 int AnalogToDigitalConversion(int ADC_RESOLUTION);
